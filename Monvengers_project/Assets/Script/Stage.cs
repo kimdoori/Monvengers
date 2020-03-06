2965e4670745295d9f08802dbff9edacdc2c9af6
kimdoori <dudu@spoqa.com>
2020-03-07 01:13:00 +0900

    Upload everything properly

---
+++ Monvengers_project/Assets/Script/Stage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Stage : MonoBehaviour {
+    public Button stageBtn;
+    public static int stageNum;
+    public static int level;
+
+    void Start ()
+    {
+        stageBtn.onClick.AddListener(ChangeScence);
+    }
+	
+	void ChangeScence()
+    {
+        DontDestroyOnLoad(this);
+        switch (stageBtn.tag)
+        {
+            case "stage1": stageNum = 1; level = 900; break;
+            case "stage2": stageNum = 2; level = 800; break;
+            case "stage3": stageNum = 3; level = 700; break;
+            case "stage4": stageNum = 4; level = 600; break;
+            case "stage5": stageNum = 5; level = 500; break;
+            case "stage6": stageNum = 6; level = 450; break;
+            case "stage7": stageNum = 7; level = 420; break;
+            case "stage8": stageNum = 8; level = 410; break;
+            case "stage9": stageNum = 9; level = 400; break;
+        }
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Monvengers_project/Assets/Script/gru.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class gru : MonoBehaviour
+{
+    int time = 0;
+    public GameObject air;
+    GameObject go;
+    RaycastHit2D hit;
+    public Transform gruPos; 
+    public int monsterHeart = 40; //몬스터의 목숨
+
+    void Start()
+    {
+        this.gruPos = this.gameObject.transform;
+        Debug.Log(this.gruPos);
+        Debug.Log(this.gruPos.position.y);
+    }
+
+    void Update()
+    {
+        time++;
+        if (time % 800 == 0)
+        {
+            Debug.Log("그루토리의 공기생성");
+            this.go = Instantiate(air) as GameObject;
+            this.go.transform.position = new Vector3(this.gruPos.position.x+1, this.gruPos.position.y, this.gruPos.position.z);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+            if (hit.collider != null && hit.collider.tag == "gr_air")
+            {
+                GameObject.Find("GameManager").GetComponent<ScoreManager>().PlusScore(10);//ScoreManager에서 PlusScore함수 호출
+                Destroy(this.go);
+            }
+        }
+
+    }
+ }
